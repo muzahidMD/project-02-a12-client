aa6cafad93dee59cc6a346138ab70da508319429
Muzahidul Islam <mujahidul.core@gmail.com>
2022-05-28 04:13:01 +0600

    find my orders using email query

--- src/page/Dashboard/MyOrders.js
+++ src/page/Dashboard/MyOrders.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrders = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([]);
+    console.log(orders)
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/orders?email=${user?.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data))
+        }
+    }, [user]);
+
     return (
         <div>
-            <h3 className='text-center text-2xl mt-2'>My Orders</h3>
+            <h3 className='text-center text-2xl mt-2'>My Orders: {orders.length}</h3>
         </div>
     );
 };
