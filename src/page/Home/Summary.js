a483cc5b70d9ad18885b1480dfc9a7e57c183688
Muzahidul Islam <mujahidul.core@gmail.com>
2022-05-28 20:22:05 +0600

    last commit of assignment

--- src/page/Home/Summary.js
+++ src/page/Home/Summary.js
@@ -1,36 +1,37 @@
+import { faFlag, faStar, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const Summary = () => {
     return (
-        <div className="stats shadow my-5 w-3/4 mx-auto">
-
-            <div className="flex-row ">
-                <div className="stat-figure text-secondary">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+        <div className='py-20'>
+            <h2 className='text-4xl text-secondary text-center uppercase font-bold'>Millions Business Trust Us</h2>
+            <h2 className='text-xl text-secondary text-center uppercase font-bold mt-10 mb-0'>We are highly care our clients!</h2>
+            <div className="stats shadow my-5 w-3/4 lg:mx-48 py-20">
+                <div className="stat ">
+                    <div className="stat-figure text-secondary">
+                        <FontAwesomeIcon icon={faFlag} className='text-4xl'></FontAwesomeIcon>
+                    </div>
+                    <div className="stat-title text-xl mb-6">Countries</div>
+                    <div className="stat-value">67</div>
                 </div>
-                <div className="stat-title">Downloads</div>
-                <div className="stat-value">31K</div>
-                <div className="stat-desc">Jan 1st - Feb 1st</div>
-            </div>
 
-            <div className="stat">
-                <div className="stat-figure text-secondary">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FontAwesomeIcon icon={faUsers} className='text-4xl'></FontAwesomeIcon>
+                    </div>
+                    <div className="stat-title text-xl mb-6">Happy client</div>
+                    <div className="stat-value">4,200+</div>
                 </div>
-                <div className="stat-title">New Users</div>
-                <div className="stat-value">4,200</div>
-                <div className="stat-desc">↗︎ 400 (22%)</div>
-            </div>
 
-            <div className="stat">
-                <div className="stat-figure text-secondary">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FontAwesomeIcon icon={faStar} className='text-4xl'></FontAwesomeIcon>
+                    </div>
+                    <div className="stat-title text-xl mb-6">Reviews</div>
+                    <div className="stat-value">1,200+</div>
                 </div>
-                <div className="stat-title">New Registers</div>
-                <div className="stat-value">1,200</div>
-                <div className="stat-desc">↘︎ 90 (14%)</div>
             </div>
-
         </div>
     );
 };
