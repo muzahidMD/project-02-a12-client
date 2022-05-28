edee81aece834e4d342c1477eec3e243d0d2ed4e
Muzahidul Islam <mujahidul.core@gmail.com>
2022-05-28 14:00:05 +0600

    my order page display my order in table

--- src/page/Dashboard/MyOrders.js
+++ src/page/Dashboard/MyOrders.js
@@ -18,6 +18,29 @@ const MyOrders = () => {
     return (
         <div>
             <h3 className='text-center text-2xl mt-2'>My Orders: {orders.length}</h3>
+            <div className="overflow-x-auto my-8">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Product Name</th>
+                            <th>Quantity</th>
+                            <th>Cancel Order</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map((order, index) =>
+                                <tr key={index}>
+                                    <th>{index + 1}</th>
+                                    <td>{order.productName}</td>
+                                    <td>{order.quantity}</td>
+                                    <td><button class="btn btn-sm">Small</button></td>
+                                </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
